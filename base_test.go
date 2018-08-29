90eecb5907bb1a5fc9a16acc27a8b11ad4f988e1
Eric Fritz <eric@eric-fritz.com>
2018-08-29 10:20:01 -0500

    Fix tests.

--- base_test.go
+++ base_test.go
@@ -12,30 +12,36 @@ type BaseSuite struct{}
 func (s *BaseSuite) TestRespond(t sweet.T) {
 	var (
 		data1 = []byte{1, 3, 5, 7, 9}
-		data2 = []byte{2, 4, 6, 8, 0}
+		data2 = []byte{2, 4, 6, 8, 0, 12}
 	)
 
 	r1 := Respond(data1)
 	Expect(r1.StatusCode()).To(Equal(http.StatusOK))
-	_, body, err := Serialize(r1)
+	headers, body, err := Serialize(r1)
 	Expect(err).To(BeNil())
 	Expect(body).To(Equal(data1))
+	Expect(headers.Get("Content-Length")).To(Equal("5"))
 
 	r2 := Respond(data2)
 	Expect(r2.StatusCode()).To(Equal(http.StatusOK))
-	_, body, err = Serialize(r2)
+	headers, body, err = Serialize(r2)
 	Expect(err).To(BeNil())
 	Expect(body).To(Equal(data2))
+	Expect(headers.Get("Content-Length")).To(Equal("6"))
 }
 
 func (s *BaseSuite) TestEmpty(t sweet.T) {
 	r1 := Empty(http.StatusNotFound)
 	Expect(r1.StatusCode()).To(Equal(http.StatusNotFound))
-	Expect(Serialize(r1)).To(BeEmpty())
+	headers, body, _ := Serialize(r1)
+	Expect(body).To(BeEmpty())
+	Expect(headers.Get("Content-Length")).To(Equal("0"))
 
 	r2 := Empty(http.StatusUnauthorized)
 	Expect(r2.StatusCode()).To(Equal(http.StatusUnauthorized))
-	Expect(Serialize(r2)).To(BeEmpty())
+	headers, body, _ = Serialize(r2)
+	Expect(body).To(BeEmpty())
+	Expect(headers.Get("Content-Length")).To(Equal("0"))
 }
 
 func (s *BaseSuite) TestJSON(t sweet.T) {
@@ -47,9 +53,10 @@ func (s *BaseSuite) TestJSON(t sweet.T) {
 
 	r := JSON(payload)
 	Expect(r.Header("Content-Type")).To(Equal("application/json"))
-	_, body, err := Serialize(r)
+	headers, body, err := Serialize(r)
 	Expect(err).To(BeNil())
 	Expect(body).To(MatchJSON(`{"prop_a":"foo","prop_b":"bar","prop_c":"baz"}`))
+	Expect(headers.Get("Content-Length")).To(Equal("46"))
 }
 
 type SampleJSON struct {
